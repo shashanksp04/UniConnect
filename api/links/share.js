21b4ffe11adc1447ca044b998b612df579d3eee1
mrid-12 <141699102+mrid-12@users.noreply.github.com>
2023-09-10 07:02:12 -0400

    Delete api directory

--- api/links/share.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router()
-
-const shareController = require('../controllers/ShareStore');
-
-router
-    .get('/find',shareController.showMembers);
-
-module.exports = router;
